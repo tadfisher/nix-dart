fcb955ea9addf80e96f3b433b995bca9ead5de72
Tad Fisher <tadfisher@gmail.com>
2021-01-22 15:40:32 -0800

    Add overlay

--- flake.nix
+++ flake.nix
@@ -11,16 +11,25 @@
   };
 
   outputs = { self, flake-utils, nixpkgs, pub2nix }:
+    let
+      buildDartPackage = pkgs:
+        let
+          yamlLib = import "${pub2nix}/yaml.nix" { inherit pkgs; };
+        in
+          pkgs.callPackage ./build.nix { inherit yamlLib; };
+    in
+    {
+      overlay = final: prev: {
+        buildDartPackage = buildDartPackage final;
+        pub2nix-lock = import "${pub2nix}/lock.nix" { pkgs = final; };
+      };
+    }
+    //
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in {
-        builders.buildDartPackage =
-          let
-            yamlLib = import "${pub2nix}/yaml.nix" { inherit pkgs; };
-          in
-            pkgs.callPackage ./build.nix { inherit yamlLib; };
-
+        builders.buildDartPackage = buildDartPackage pkgs;
         packages.pub2nix-lock = import "${pub2nix}/lock.nix" { inherit pkgs; };
       }
     );
